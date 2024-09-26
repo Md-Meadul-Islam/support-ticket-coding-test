91e44c20741361f0a88f3200996fdb02ca3e5d60
Md-Meadul-Islam <mdmeadulislam@gmail.com>
2024-09-26 21:53:07 +0600

    make controller

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin | Dashboard</title>
+    <link rel="stylesheet" href="{{asset('files/bootstrap.css')}}">
+</head>
+<body>
+    
+    <script src="{{asset('files/bootstrap5.3.3.min.js')}}"></script>
+    <script src="{{asset('files/jquery.min.css')}}"></script>
+</body>
+</html>
